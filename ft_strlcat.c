a72916bb55a1008cac354d9cf986d50f3395ce14
Firas Lafi <flafi@3-B-2.42heilbronn.de>
2023-03-29 01:33:06 +0200

    vvvv

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,30 +6,31 @@
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 15:14:18 by flafi             #+#    #+#             */
-/*   Updated: 2023/03/29 01:13:01 by flafi            ###   ########.fr       */
+/*   Updated: 2023/03/29 01:30:44 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-  int i;
-  int j;
-  int init_dstsize;
+  size_t i;
+  size_t j;
+  size_t init_dstsize;
   
   init_dstsize = ft_strlen(dst);
   i = 0;
   j = init_dstsize;
-
-  while(src[i] != '\0' && j < init_dstsize - 1)
+  if(dstsize == 0)
+    return(ft_strlen(src));
+  while(src[i] != '\0' && j < (dstsize - 1))
     {
       dst[j] = src[i];
       j++;
       i++;
     }
-  if(i < (int)dstsize)
-    *dst = '\0';
-  if (init_dstsize < (int)dstsize)
+  if(i < dstsize)
+    dst[j] = '\0';
+  if (init_dstsize < dstsize)
       return (init_dstsize + ft_strlen(src));
   else
       return(dstsize + ft_strlen(src));

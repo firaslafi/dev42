79ab87a420c5f5e179d247bba180bd82fcf82c65
Firas Lafi <flafi@3-B-7.42heilbronn.de>
2023-04-01 05:36:49 +0200

    vv

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,40 +6,33 @@
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 17:44:27 by flafi             #+#    #+#             */
-/*   Updated: 2023/04/01 02:03:49 by flafi            ###   ########.fr       */
+/*   Updated: 2023/04/01 05:36:06 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 void    *ft_memmove(void *dest, const void *src, size_t n)
 {
-  size_t lensrc;
-  char *dsttmp;
-  char *srctmp;
   size_t i;
-
+  char *chr;
+  
+  chr = NULL;
+  i = 0;
   if(src == NULL && dest == NULL)
     return(dest);
-  i = 0;
-  lensrc = ft_strlen(src);
-
-  dsttmp = calloc(lensrc, 1);
-  srctmp = calloc(lensrc, 1);
-
-  //copy the src to srctmp
-  while(lensrc--)
+  while(((char *)src)[i])
     {
-      *srctmp = ((char *)src)[i];
-      srctmp++;
+      *chr = ((char *)src)[i];
       i++;
+      chr++;
     }
-// moving from srctm to dest
+
+  i = 0;
   while(n--)
     {
-      *dsttmp = *srctmp;
-      srctmp++;
-      dsttmp++;
+      ((char *)dest)[i] = chr[i];
+      i++;
     }
-  
   return (dest);
 }

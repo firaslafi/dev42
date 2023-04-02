a4296bce4edd3e92c61aafb2255b2c673ecb15ed
Firas Lafi <flafi@3-B-7.42heilbronn.de>
2023-04-02 04:22:05 +0200

    2 fixed

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,50 +6,12 @@
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 17:44:27 by flafi             #+#    #+#             */
-/*   Updated: 2023/04/02 02:42:52 by flafi            ###   ########.fr       */
+/*   Updated: 2023/04/02 02:44:37 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
-#include <string.h>
-
-// void    *ft_memmove(void *dest, const void *src, size_t n)
-// {
-//   char *chrd;
-//   char *chrs;
-
-//   if(src == NULL && dest == NULL)
-//     return(dest);
-  
-//   chrd = (char *)dest;
-//   chrs = (char *)src;
-
-// // printf("test\n");
-//   if (dest > src) 
-//   {
-//     while(n > 0)
-//     {
-//       chrd[n] = chrs[n];
-//       n--;
-//     }
-//   }
-//   else
-//     {
-//       while(n > 0)
-//       { 
-//         n--;
-//         *chrd++ = *chrs++;
-//         //  printf("case 2  > %s  |  %s\n",dest, src);
-//         //*chrd = *chrs;
-//         //chrd++;
-//         //chrs++;
-//       }
-//     }
-
-
-//   return (dest);
-// }
 
 void    *ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -67,13 +29,3 @@ void    *ft_memmove(void *dest, const void *src, size_t n)
       d[n] = s[n];
   return (d);
 }
-
-
-// int main()
-// {
-
-// 	char	*dest;
-//   char	src[] = "lorem ipsum dolor sit amet";
-//   printf("%s\n",ft_memmove(dest, src, 8));
-//   return 0;
-// }
--- libft.h
+++ libft.h
@@ -24,5 +24,6 @@ size_t ft_strlcat(char *dst, const char *src, size_t dstsize);
 void *ft_calloc(size_t nelem, size_t elemsize);
 char *ft_strdup(const char *s);
 void    *ft_memmove(void *dest, const void *src, size_t n);
+char *ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif

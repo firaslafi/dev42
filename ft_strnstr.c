1d251b0b9c49a65a7f9839cf596d90ed14ba8a95
Firas Lafi <flafi@3-E-6.42heilbronn.de>
2023-04-05 14:21:30 +0200

    vv

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,50 @@
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 03:33:18 by flafi             #+#    #+#             */
-/*   Updated: 2023/03/27 03:35:53 by flafi            ###   ########.fr       */
+/*   Updated: 2023/04/05 14:06:23 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-char *strnstr(const char *haystack, const char *needle, size_t len)
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    while
+  size_t i;
+  size_t j;
+  size_t needlength;
+  size_t found;
+  char *result;
+  i = 0;
+  j = 0;
+  found = 0;
+  needlength = ft_strlen(needle);
+  if(len <= 0)
+    return (NULL);
+  if(!needle)
+    return ((char *)haystack);
+  while(i < len)
+  {
+    if(haystack[i] != needle[j])
+      i++;
+    else
+    {
+      j = 0;
+      while(haystack[i] == needle[j])
+        {
+          if(j == needlength - 1)
+          {
+            found = 1;
+            result = (char *)&haystack[i-j];
+            break;
+          }
+          i++;
+          j++;
+        }
+        if (found)
+         break;
+    }
+  }
+  if (!found)
+    return(NULL);
+  else
+    return(result);
 }
